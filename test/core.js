30d34068ca2db1474de10d88c79051742b3a3374
itsatony <i@itsatony.com>
2015-10-31 11:43:50 +0100

    moved most pieces to promises. introduced metaEvents for better logging/debugging

--- test/core.js
+++ test/core.js
@@ -3,8 +3,6 @@ var Q = require('q');
 var expect = require('expect');
 var nAPI = require('../lib/napi.js');
 
-var Lg = require('lg'); 
-var log = new Lg({log2console:true, logLevel:1000});
 
 console.deb = function(obj) {
 	var debug = util.inspect(obj, { showHidden: true, depth: null, colors: true });
@@ -25,18 +23,41 @@ var adapterConfig_mongoDB_users = {
 
 
 
-var api = new nAPI.ApiCore('testapi');
-api._addResource('users', [ adapterConfig_mongoDB_users ], ready);
+api = null;
+
+nAPI.ApiCore('testapi', null)
+	.then(
+		function(myApi) {
+			api = myApi;
+			return api._addResource('users', [ adapterConfig_mongoDB_users ]);
+		}
+	).then(
+		ready
+	).catch(
+		function(err) {
+			console.error('####### ERROR ##### ', err.stack || err);
+			process.exit(1);
+		}
+	)
+;
+
 
 
 function ready() {
 	console.log('----------------READY-------------------');
-	var myAllow = nAPI.Utils.allows.createWhitelistAllow([nAPI.Defaults.allows.userIsLoggedIn]);
+	var myAllow = nAPI.Utils.allows.createBlacklistAllow([]);
+	// name, steps, description, parameters, tests
 	api.users.get._addSpecifier(
 		'byId',
 		[
-			[ 'allow', myAllow ],
-			[ 'action', nAPI.Adaptors.mongoDB.actions.get.byId ]
+			{ 
+				name: 'allow', 
+				resolveHandler: myAllow
+			},
+			{ 
+				name: 'action', 
+				resolveHandler: nAPI.Adaptors.mongoDB.actions.get.byId 
+			},
 		],
 		'Get a User by their unique ID',
 		{
@@ -60,9 +81,25 @@ function ready() {
 			}
 		}
 	);
-	api.users.get.byId({ _id: '4f39e4f12ce81f09340002b9' }, { limit:1 }, function(err, results) { console.log('--->>>got answer:', err, results); });
-	var a = log.njson(api, 'napi_init');
-	console.log(JSON.stringify(api.getDocumentation(), null, '  '));
+	// console.log('+++++++++++++++++++\n');
+	// console.log(api.users.get.byId);
+	api.users.get.byId(
+		{ _id: '4f39e4f12ce81f09340002b9' }, 
+		{ limit:1 }
+	).then(
+		function(result) {
+			console.log(':::::: API ANSWERED :::::\n', result);
+		}
+	).catch(
+		function(err) {
+			console.error('::::::: ERROR::::\n', err.stack || err);
+		}
+	).finally(
+		function() {
+			process.exit();
+		}
+	);
+	// console.log(JSON.stringify(api.getDocumentation(), null, '  '));
 };
 
 
